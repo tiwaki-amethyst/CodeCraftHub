d3fe2a67e534f3d9ebde29b221951f7a6379d357
tiwaki-amethyst <tiwaki-amethyst@gmail.com>
2025-10-04 04:49:13 -0400

    Fix UsersModule JwtModule dependency

--- user-management-service/src/modules/users/users.module.ts
+++ user-management-service/src/modules/users/users.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret',
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1h' }
+    })
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
